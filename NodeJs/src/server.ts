300964ccebca8391a581ea59cbb14e45af9928a9
Alex <alexander.g@btigroup.io>
2019-08-15 12:56:03 +0300

    Added HTTPS support

--- NodeJs/src/server.ts
+++ NodeJs/src/server.ts
@@ -1,4 +1,14 @@
+import http from 'http';
+// import https from 'https';
+
 import app from './app';
 
-app.listen(3000);
-console.log('Server is online');
+const HOST = 'localhost';
+const HTTP_PORT = 3000;
+// const HTTPS_PORT = 3001;
+
+const httpServer = http.createServer(app.callback()).listen(HTTP_PORT, HOST, () => {console.log(`Listening on http://${HOST}:${HTTP_PORT}...`);});
+// getting error ERR_SSL_VERSION_OR_CIPHER_MISMATCH when accessing HTTPS
+// so disabling for now
+// const httpsServer = https.createServer(app.callback()).listen(HTTPS_PORT, HOST, () => {console.log(`Listening on https://${HOST}:${HTTPS_PORT}...`);});
+
